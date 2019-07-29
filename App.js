d0dc78c3ac064f757c36d2a1e5b41b3af92ce453
Johan Navarro <21sebas12@gmail.com>
2019-07-28 23:28:48 -0500

    Is set the preloader for ocupate all dysplay

--- App.js
+++ App.js
@@ -5,26 +5,28 @@ import PreLoader from './application/components/PreLoader';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app! Johan</Text>
-      <AppButton
-        title={"text"}
-        bgColor={"rgba(111,38,74,0.7)"}
-        action={()=>console.log(1)}
-        iconName = {"sign-in"}
-        iconSize = {30}
-        iconColor ={"#fff"}
-      /> */}
+    <>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Johan</Text>
+        <AppButton
+          title={"text"}
+          bgColor={"rgba(111,38,74,0.7)"}
+          action={()=>console.log(1)}
+          iconName = {"sign-in"}
+          iconSize = {30}
+          iconColor ={"#fff"}
+        /> 
+      </View>
       <PreLoader/>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex : 1, //with this we indicate that it takes as much space as it can
+    flexDirection : 'column',  //with this we indicate that it takes much space as it cana vertical 
+    justifyContent : 'center', //we want to center all the elements
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
